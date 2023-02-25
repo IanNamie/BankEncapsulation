fb263af945b4ebbcba45a1fcda824b394fb4593f
Ian Namie <Iannamie82@gmail.com>
2023-02-25 15:45:15 -0600

    finished exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var bankAccount = new BankAccount();
+            double deposite = 0;
+            //Allow the user of the application to Deposit money and retrieve their balance through the console.
+            
+                Console.WriteLine("Welcome to Namie banks of America, how much would you like to deposite today?");
+                deposite = double.Parse(Console.ReadLine());
+                bankAccount.Deposite(deposite);
+            
+
+            Console.WriteLine($"Great you deposited ${deposite} into your account.");
+            Console.WriteLine($"That makes your balence $ {bankAccount.GetBalance()}. Thanks for coming to Namie Banking!");
+
+
         }
     }
 }
